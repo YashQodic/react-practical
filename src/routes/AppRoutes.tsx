8212423874621abbf5f5fe2bc19d4e666aef0b5f
Yash <yash@qodictechnosoft.com>
2026-01-21 15:01:01 +0530

    Created header UI

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router";
 import Login from "../features/auth/pages/Login";
 import Register from "../features/auth/pages/Register";
 import ForgotPassword from "../features/auth/pages/ForgotPassword";
+import Layout from "../components/layout/Layout";
 
 export default function AppRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route element={<Layout />}>
+                <Route path="/" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+            </Route>
             <Route path="/*" element={<div>404 Not Found</div>}></Route>
         </Routes>
     );
