7b0d667ae6ad385afd922c06d7802046a2d617c6
Yash <yash@qodictechnosoft.com>
2026-01-21 17:29:40 +0530

    Implemented footer

--- src/features/Product/pages/Product.tsx
+++ src/features/Product/pages/Product.tsx
@@ -1,6 +1,6 @@
 function Product() {
   return (
-    <div className="p-4">
+    <div className="p-4 h-screen">
       <h1 className="text-2xl font-bold mb-4">Product Page</h1>
       <p>Welcome to the Product page. Here you can find information about our products.</p>
     </div>
--- src/features/contact/pages/Contact.tsx
+++ src/features/contact/pages/Contact.tsx
@@ -1,6 +1,6 @@
 function Contact() {
   return (
-    <div className="p-4">
+    <div className="p-4 h-screen">
       <h1 className="text-2xl font-bold mb-4">Contact Page</h1>
       <p>
         Welcome to the Contact page. Here you can find information about our
